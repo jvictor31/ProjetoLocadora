f7004eef8b5ba1e65fc84fa995e3abfd080b3b3a
jvictor31 <57421617+jvictor31@users.noreply.github.com>
2021-06-18 10:41:49 -0300

    Create Alugar.java

---
+++ Alugar.java
@@ -0,0 +1,111 @@
+package com.mycompany.model;
+
+import com.mycompany.conexao.EntidadeBase;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "alugar")
+@NamedQueries({
+    @NamedQuery(name = "Alugar.findAll", query = "SELECT a FROM Alugar a")})
+public class Alugar implements Serializable, EntidadeBase {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "data")
+    private String data;
+    @Column(name = "valor")
+    private String valor;
+    @Column(name = "prazoEntrega")
+    private String prazoEntrega;
+    @JoinColumn(name = "id_cliente", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cliente idCliente;
+
+    public Alugar() {
+    }
+
+    public Alugar(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getPrazoEntrega() {
+        return prazoEntrega;
+    }
+
+    public void setPrazoEntrega(String prazoEntrega) {
+        this.prazoEntrega = prazoEntrega;
+    }
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alugar)) {
+            return false;
+        }
+        Alugar other = (Alugar) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.model.Alugar[ id=" + id + " ]";
+    }
+    
+}
