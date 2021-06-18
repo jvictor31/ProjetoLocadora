78f13e46b1ae32050a9a6be77d8775a3ba6d5c8f
jvictor31 <57421617+jvictor31@users.noreply.github.com>
2021-06-18 10:43:27 -0300

    Create Endereço.java

---
+++ Endereço.java
@@ -0,0 +1,91 @@
+package com.mycompany.model;
+
+import com.mycompany.conexao.EntidadeBase;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "endere\u00e7o")
+@NamedQueries({
+    @NamedQuery(name = "Endere\u00e7o.findAll", query = "SELECT e FROM Endere\u00e7o e")})
+public class Endereço implements Serializable, EntidadeBase {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "rua")
+    private String rua;
+    @JoinColumn(name = "id_cliente", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cliente idCliente;
+
+    public Endereço() {
+    }
+
+    public Endereço(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Endereço)) {
+            return false;
+        }
+        Endereço other = (Endereço) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.model.Endere\u00e7o[ id=" + id + " ]";
+    }
+    
+}
