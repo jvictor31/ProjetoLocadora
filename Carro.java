0b174bfb2a76039b02d631c61208cfa5982549f6
jvictor31 <57421617+jvictor31@users.noreply.github.com>
2021-06-18 10:42:26 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,91 @@
+package com.mycompany.model;
+
+import com.mycompany.conexao.EntidadeBase;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "carro")
+@NamedQueries({
+    @NamedQuery(name = "Carro.findAll", query = "SELECT c FROM Carro c")})
+public class Carro implements Serializable, EntidadeBase {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "nomeCarro")
+    private String nomeCarro;
+    @JoinColumn(name = "id_cliente", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cliente idCliente;
+
+    public Carro() {
+    }
+
+    public Carro(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCarro() {
+        return nomeCarro;
+    }
+
+    public void setNomeCarro(String nomeCarro) {
+        this.nomeCarro = nomeCarro;
+    }
+
+    public Cliente getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Cliente idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Carro)) {
+            return false;
+        }
+        Carro other = (Carro) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.model.Carro[ id=" + id + " ]";
+    }
+    
+}
